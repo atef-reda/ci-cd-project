6373d08c12e05403ae0078d5846aaa34650faad5
atef reda <89020766+atef-reda@users.noreply.github.com>
2025-10-12 06:52:11 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0c55b159cbfafe1f0" # Example Ubuntu AMI
+  instance_type = "t2.micro"
+  key_name      = "my-key"
+
+  tags = {
+    Name = "jenkins-ec2"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.demo.public_ip
+}
 
